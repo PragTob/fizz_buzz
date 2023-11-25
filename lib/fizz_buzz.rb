c00ca8e97cbad63c895f4b77b659641f62ca3fdc
Tobias Pfeiffer <pragtob@gmail.com>
2023-11-25 15:43:29 +0100

    Introduction of FizzBuzz rules

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -4,36 +4,40 @@
 module FizzBuzz
   module_function
 
-  FIZZ_NUMBER = 3
-  FIZZ_TEXT = "Fizz"
+  # A rule in the game of FizzBuzz as in, put out this text if it is divisible by x
+  class Rule
+    def initialize(output, applicalbe_divisible_by)
+      @output = output
+      @applicalbe_divisible_by = applicalbe_divisible_by
+    end
 
-  BUZZ_NUMBER = 5
-  BUZZ_TEXT = "Buzz"
+    def apply(number)
+      @output if divisible_by?(number, @applicalbe_divisible_by)
+    end
 
-  FIZZ_BUZZ_TEXT = FIZZ_TEXT + BUZZ_TEXT
+    def divisible_by?(number, divisor)
+      number % divisor == 0
+    end
+  end
 
-  def fizz_buzz(number)
-    fizz = divisible_by?(number, FIZZ_NUMBER)
-    buzz = divisible_by?(number, BUZZ_NUMBER)
+  DEFAULT_RULES = [
+    Rule.new("Fizz", 3),
+    Rule.new("Buzz", 5)
+  ].freeze
 
-    if fizz & buzz
-      FIZZ_BUZZ_TEXT
-    elsif fizz
-      FIZZ_TEXT
-    elsif buzz
-      BUZZ_TEXT
+  def fizz_buzz(number, rules = DEFAULT_RULES)
+    applied_rules = rules.filter_map { |rule| rule.apply(number) }
+
+    if applied_rules.any?
+      applied_rules.join
     else
       number
     end
   end
 
-  def run
+  def run(rules = DEFAULT_RULES)
     1.upto(100) do |number|
-      puts fizz_buzz(number)
+      puts fizz_buzz(number, rules)
     end
-  end
-
-  def divisible_by?(number, divisor)
-    number % divisor == 0
   end
 end
